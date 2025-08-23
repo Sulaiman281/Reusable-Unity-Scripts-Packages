2e559514fff42ab4a9c1c991494d8cec71ba7192
Syed Suleiman <55351798+Sulaiman281@users.noreply.github.com>
2025-08-23 15:26:15 +0500

    added generic nodes bindable management pattern,
    enhance nodes bindable
    so user can pass any kind of data model.
    and be able to track it's new data.

---
+++ Assets/WitShells/DesignPatterns/Runtime/Scripts/Core/BindableNodes.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+
+namespace WitShells.DesignPatterns.Core
+{
+    public abstract class NodeController<T> : Builder<T>, IEquatable<T>
+    {
+        private Bindable<T> _currentNode = new Bindable<T>();
+
+        public NodeController(T node)
+        {
+            _currentNode.Value = node;
+        }
+
+        public Bindable<T> CurrentNode => _currentNode;
+
+        public virtual void UpdateNode(T newValue)
+        {
+            _currentNode.Value = newValue;
+        }
+
+        public virtual bool Equals(T other)
+        {
+            if (other == null) return false;
+            return _currentNode.Value.Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return _currentNode.Value.GetHashCode();
+        }
+        public override T Build()
+        {
+            return _currentNode.Value;
+        }
+    }
+
+    public interface INodeManager<T>
+    {
+        public bool AddNode(NodeController<T> node);
+        public bool RemoveNode(NodeController<T> node);
+        public IEnumerable<NodeController<T>> GetAllNodes();
+    }
+
+    public class UniqueNodesManager<T, TKey> : INodeManager<T>
+    {
+        private Dictionary<TKey, NodeController<T>> _uniqueNodes = new Dictionary<TKey, NodeController<T>>();
+        private readonly Func<T, TKey> _keySelector;
+
+        public UniqueNodesManager(Func<T, TKey> keySelector)
+        {
+            _keySelector = keySelector ?? throw new ArgumentNullException(nameof(keySelector));
+        }
+
+        public bool AddNode(NodeController<T> node)
+        {
+            var key = _keySelector(node.CurrentNode.Value);
+            if (!_uniqueNodes.ContainsKey(key))
+            {
+                _uniqueNodes[key] = node;
+                return true;
+            }
+            return false;
+        }
+
+        public bool RemoveNode(NodeController<T> node)
+        {
+            var key = _keySelector(node.CurrentNode.Value);
+            return _uniqueNodes.Remove(key);
+        }
+
+        public NodeController<T> GetNode(T value)
+        {
+            var key = _keySelector(value);
+            _uniqueNodes.TryGetValue(key, out var node);
+            return node;
+        }
+
+        public NodeController<T> GetNodeByKey(TKey key)
+        {
+            _uniqueNodes.TryGetValue(key, out var node);
+            return node;
+        }
+
+        public IEnumerable<NodeController<T>> GetAllNodes()
+        {
+            return _uniqueNodes.Values;
+        }
+    }
+
+    public class NodesManager<T> : INodeManager<T>
+    {
+        private List<NodeController<T>> _nodes = new List<NodeController<T>>();
+
+        public bool AddNode(NodeController<T> node)
+        {
+            if (!_nodes.Contains(node))
+            {
+                _nodes.Add(node);
+                return true;
+            }
+            return false;
+        }
+
+        public bool RemoveNode(NodeController<T> node)
+        {
+            return _nodes.Remove(node);
+        }
+
+        public NodeController<T> GetNode(T value)
+        {
+            return _nodes.Find(node => node.CurrentNode.Value.Equals(value));
+        }
+
+        public IEnumerable<NodeController<T>> GetAllNodes()
+        {
+            return _nodes;
+        }
+    }
+}
